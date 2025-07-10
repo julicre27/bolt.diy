bfdb00ac071253b53fc33b787b20ad9743d948ff
julicre27 <julicre@gmail.com>
2025-07-10 12:42:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -93,6 +93,7 @@ export default defineConfig((config) => {
       },
  server: {
       host: 0.0.0.0,
+      port: 5173,
       strictPort: true,
       allowedHosts: ['app-boltdiy.yazeuk.easypanel.host'],
 },
